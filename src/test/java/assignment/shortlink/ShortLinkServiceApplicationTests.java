5c78f8b7d417adc5244b693cac503227fbe3f252
Krieit <jasonsassaman71@gmail.com>
2024-10-27 18:15:56 -0400

    Initial commit: Create Spring Boot project structure

---
+++ src/test/java/assignment/shortlink/ShortLinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.shortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
